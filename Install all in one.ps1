70cf266f2289b292af0e9e005d0dca0e5cf8920e
MBN <35900649+MBNpro-ir@users.noreply.github.com>
2024-11-05 00:59:36 +0330

    Update v1.4.2

--- Install all in one.ps1
+++ Install all in one.ps1
@@ -1,70 +1,36 @@
 ﻿function Show-Error {
-    param (
-        [string]$message
-    )
+    param ([string]$message)
     Write-Host "Error: $message" -ForegroundColor Red
 }
 
 function Show-Status {
-    param (
-        [string]$message
-    )
+    param ([string]$message)
     Write-Host "Status: $message" -ForegroundColor Green
 }
 
 function Test-SpotifyInstallation {
-    param (
-        [string]$path
-    )
+    param ([string]$path)
     return Test-Path $path
 }
 
 function Test-SpicetifyInstallation {
-    param (
-        [string]$path
-    )
+    param ([string]$path)
     return Test-Path $path
 }
 
 function Test-ProcessRunning {
-    param (
-        [string]$name
-    )
+    param ([string]$name)
     $process = Get-Process -Name $name -ErrorAction SilentlyContinue
     return $null -ne $process
 }
 
 function Test-InstallerIntegrity {
-    param (
-        [string]$path
-    )
+    param ([string]$path)
     try {
         $fileInfo = Get-Item $path
         return $fileInfo.Length -gt 0
     } catch {
         return $false
-    }
-}
-
-function Cleanup-Downloads {
-    $installerPath = "$env:TEMP\SpotifyFullSetup.exe"
-    $extensionsDir = "$env:APPDATA\spicetify\Extensions"
-    $tempFiles = Get-ChildItem -Path "$env:TEMP" -File -Recurse
-
-    if ($tempFiles.Count -gt 0) {
-        Remove-Item $tempFiles -Force
-        Show-Status "All temporary files cleaned up."
-    } else {
-        Show-Status "No temporary files to clean up."
-    }
-
-    if (Test-Path $installerPath) {
-        Remove-Item $installerPath -Force
-        Show-Status "Spotify installer cleaned up."
-    }
-    if (Test-Path $extensionsDir) {
-        Remove-Item $extensionsDir\* -Force
-        Show-Status "Extensions cleaned up."
     }
 }
 
@@ -86,9 +52,9 @@ function Install-Spotify {
             Start-Process -FilePath $installerPath -ArgumentList "/silent" -NoNewWindow
 
             $attempt = 0
-            $maxAttempts = 15
+            $maxAttempts = 27
             do {
-                Start-Sleep -Seconds 5
+                Start-Sleep -Seconds 27
                 $attempt++
                 if ($attempt -ge $maxAttempts) {
                     Show-Error "Spotify failed to launch within the expected time."
@@ -128,44 +94,187 @@ function Install-Spicetify {
     }
 }
 
-function Install-Extension {
-    param (
-        [string]$extensionUrl,
-        [string]$extensionName
-    )
+function Uninstall-Spotify {
+    try {
+        $spotifyPath = "$env:APPDATA\Spotify"
+        if (Test-Path $spotifyPath) {
+            Stop-Process -Name "Spotify" -Force -ErrorAction SilentlyContinue
+            Remove-Item -Path $spotifyPath -Recurse -Force -ErrorAction Stop
+            Show-Status "Spotify uninstalled successfully."
+        } else {
+            Show-Error "Spotify is not installed."
+        }
+    } catch {
+        Show-Error "Failed to uninstall Spotify."
+    }
+}
+
+function Uninstall-Spicetify {
+    try {
+        spicetify restore
+        Remove-Item -Path "$env:APPDATA\spicetify" -Recurse -Force -ErrorAction Stop
+        Remove-Item -Path "$env:LOCALAPPDATA\spicetify" -Recurse -Force -ErrorAction Stop
+        Show-Status "Spicetify and its data uninstalled successfully."
+    } catch {
+        Show-Error "Failed to uninstall Spicetify."
+    }
+}
+
+function Uninstall-Extension {
+    param ([string]$extensionName)
+    try {
+        spicetify config extensions $extensionName
+        Show-Status "$extensionName extension uninstalled successfully."
+    } catch {
+        Show-Error "Failed to uninstall $extensionName extension."
+    }
+}
+
+function Install-Extension-LoopyLoop {
+    try {
+        spicetify config extensions loopyLoop.js
+        Show-Status "LoopyLoop extension configured."
+    } catch {
+        Show-Error "Failed to configure LoopyLoop extension."
+    }
+}
+
+function Install-Extension-PopupLyrics {
+    try {
+        spicetify config extensions popupLyrics.js
+        Show-Status "PopupLyrics extension configured."
+    } catch {
+        Show-Error "Failed to configure PopupLyrics extension."
+    }
+}
+
+function Install-Extension-ShufflePlus {
+    try {
+        spicetify config extensions shuffle+.js
+        Show-Status "ShufflePlus extension configured."
+    } catch {
+        Show-Error "Failed to configure ShufflePlus extension."
+    }
+}
+
+function Install-Extension-lyrics-plus {
+    try {
+        spicetify config custom_apps lyrics-plus
+        Show-Status "lyrics-plus extension configured."
+    } catch {
+        Show-Error "Failed to configure lyrics-plus extension."
+    }
+}
+
+function Install-Extension-new-releases {
+    try {
+        spicetify config custom_apps new-releases
+        Show-Status "new-releases extension configured."
+    } catch {
+        Show-Error "Failed to configure new-releases extension."
+    }
+}
+
+function Install-Extension-HistoryInSidebar {
     try {
         $extensionsDir = "$env:APPDATA\spicetify\Extensions"
-
         if (-Not (Test-Path $extensionsDir)) {
             New-Item -ItemType Directory -Path $extensionsDir -Force | Out-Null
         }
 
-        Invoke-WebRequest -Uri $extensionUrl -OutFile "$extensionsDir\$extensionName" -ErrorAction Stop
-        Show-Status "$extensionName configured."
-
-        spicetify apply
-        spicetify backup apply
-        Show-Status "Spicetify configurations applied after installing $extensionName."
+        Invoke-WebRequest -Uri "https://codeload.github.com/Bergbok/Spicetify-Creations/zip/refs/heads/dist/history-in-sidebar" -OutFile "$extensionsDir\history-in-sidebar.zip" -ErrorAction Stop
+        Show-Status "HistoryInSidebar extension downloaded."
+        
+        spicetify config custom_apps history-in-sidebar
+        Show-Status "HistoryInSidebar extension configured."
     } catch {
-        Show-Error "Failed to download or configure $extensionName."
+        Show-Error "Failed to download or configure HistoryInSidebar extension."
     }
 }
 
-function Execute-All {
-    Install-Spotify
-    Install-Spicetify
-    Install-Extension -extensionUrl "https://raw.githubusercontent.com/Bergbok/Spicetify-Creations/refs/heads/dist/auto-skip-tracks-by-duration/auto-skip-tracks-by-duration.js" -extensionName "auto-skip-tracks-by-duration.js"
-    Install-Extension -extensionUrl "https://codeload.github.com/Bergbok/Spicetify-Creations/zip/refs/heads/dist/history-in-sidebar" -extensionName "history-in-sidebar.zip"
+function Install-AllExtensions {
+    Show-Status "Installing all extensions..."
+
+    Install-Extension-LoopyLoop
+    Install-Extension-PopupLyrics
+    Install-Extension-ShufflePlus
+    Install-Extension-lyrics-plus
+    Install-Extension-new-releases
+    Install-Extension-HistoryInSidebar
+
+    spicetify apply
+    Show-Status "All extensions installed and configurations applied."
 }
 
-function Apply-SpicetifyConfigs {
-    if (Test-SpicetifyInstallation "$env:LOCALAPPDATA\spicetify") {
-        spicetify apply
-        spicetify backup apply
-        Show-Status "Spicetify configurations applied."
-    } else {
-        Show-Error "Spicetify is not installed. Please install it first."
-    }
+function Show-ExtensionsMenu {
+    do {
+        Write-Host "======= Extensions Menu ======="
+        Write-Host "1. Install LoopyLoop Extension"
+        Write-Host "2. Install PopupLyrics Extension"
+        Write-Host "3. Install ShufflePlus Extension"
+        Write-Host "4. Install lyrics-plus Extension"
+        Write-Host "5. Install new-releases Extension"
+        Write-Host "6. Install HistoryInSidebar Extension"
+        Write-Host "7. Install All Extensions"
+        Write-Host "8. Return to Main Menu"
+        Write-Host "================================"
+        
+        $choice = Read-Host "Select an option"
+
+        switch ($choice) {
+            "1" { Install-Extension-LoopyLoop }
+            "2" { Install-Extension-PopupLyrics }
+            "3" { Install-Extension-ShufflePlus }
+            "4" { Install-Extension-lyrics-plus }
+            "5" { Install-Extension-new-releases }
+            "6" { Install-Extension-HistoryInSidebar }
+            "7" { Install-AllExtensions }
+            "8" { return }
+            default { Write-Host "Invalid selection, please try again." -ForegroundColor Yellow }
+        }
+    } while ($true)
+}
+
+function Show-UninstallMenu {
+    do {
+        Write-Host "======= Uninstall Menu ======="
+        Write-Host "1. Uninstall Spotify"
+        Write-Host "2. Uninstall Spicetify"
+        Write-Host "3. Uninstall Extension"
+        Write-Host "4. Return to Main Menu"
+        Write-Host "=============================="
+        
+        $choice = Read-Host "Select an option"
+
+        switch ($choice) {
+            "1" { Uninstall-Spotify }
+            "2" { Uninstall-Spicetify }
+            "3" {
+                Write-Host "Select the extension to uninstall:"
+                Write-Host "1. LoopyLoop"
+                Write-Host "2. PopupLyrics"
+                Write-Host "3. ShufflePlus"
+                Write-Host "4. lyrics-plus"
+                Write-Host "5. new-releases"
+                Write-Host "6. HistoryInSidebar"
+                Write-Host "7. Return"
+                
+                $extChoice = Read-Host "Select an option"
+                switch ($extChoice) {
+                    "1" { Uninstall-Extension "loopyLoop.js" }
+                    "2" { Uninstall-Extension "popupLyrics.js" }
+                    "3" { Uninstall-Extension "shuffle+.js" }
+                    "4" { Uninstall-Extension "lyrics-plus" }
+                    "5" { Uninstall-Extension "new-releases" }
+                    "6" { Uninstall-Extension "history-in-sidebar" }
+                    "7" { continue }
+                    default { Write-Host "Invalid selection, please try again." -ForegroundColor Yellow }
+                }
+            }
+            "4" { return }
+            default { Write-Host "Invalid selection, please try again." -ForegroundColor Yellow }
+        }
+    } while ($true)
 }
 
 function Show-Menu {
@@ -173,12 +282,11 @@ function Show-Menu {
         Write-Host "========================="
         Write-Host "1. Install Spotify"
         Write-Host "2. Install Spicetify"
-        Write-Host "3. Install auto-skip-tracks-by-duration Extension"
-        Write-Host "4. Install history-in-sidebar Extension"
-        Write-Host "5. Spicetify Apply Configs"
-        Write-Host "6. Execute All Steps"
-        Write-Host "7. Cleanup Downloads"
-        Write-Host "8. Exit"
+        Write-Host "3. Extensions Menu"
+        Write-Host "4. Spicetify Apply Configs"
+        Write-Host "5. Cleanup Downloads"
+        Write-Host "6. Uninstall Menu"
+        Write-Host "7. Exit"
         Write-Host "========================="
         
         $choice = Read-Host "Select an option"
@@ -186,15 +294,11 @@ function Show-Menu {
         switch ($choice) {
             "1" { Install-Spotify }
             "2" { Install-Spicetify }
-            "3" { Install-Extension -extensionUrl "https://raw.githubusercontent.com/Bergbok/Spicetify-Creations/refs/heads/dist/auto-skip-tracks-by-duration/auto-skip-tracks-by-duration.js" -extensionName "auto-skip-tracks-by-duration.js" }
-            "4" { Install-Extension -extensionUrl "https://codeload.github.com/Bergbok/Spicetify-Creations/zip/refs/heads/dist/history-in-sidebar" -extensionName "history-in-sidebar.zip" }
-            "5" { Apply-SpicetifyConfigs }
-            "6" { Execute-All }
-            "7" { Cleanup-Downloads }
-            "8" { 
-                Write-Host "Exiting program..." 
-                Exit
-            }
+            "3" { Show-ExtensionsMenu }
+            "4" { spicetify apply }
+            "5" { Cleanup-Downloads }
+            "6" { Show-UninstallMenu }
+            "7" { exit }
             default { Write-Host "Invalid selection, please try again." -ForegroundColor Yellow }
         }
     } while ($true)
