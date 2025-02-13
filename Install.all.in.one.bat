66c62672995ec18cefa9fc98022efacdb099a90b
MBN <farid.faridi1382@gmail.com>
2025-02-13 20:29:34 +0330

    Create Install.all.in.one.bat

---
+++ Install.all.in.one.bat
@@ -0,0 +1,16 @@
+@echo off
+set param=
+set url='https://github.com/MBNpro-ir/All-in-one-spotify/releases/download/V2.0.0/Install.all.in.one.ps1'
+
+:: mirror URL
+set url2='https://raw.githubusercontent.com/MBNpro-ir/All-in-one-spotify/main/Install.all.in.one.ps1'
+
+:: Set TLS 1.2 for secure download (important for HTTPS)
+set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
+
+:: Execute PowerShell command to download and run the script
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
+-Command %tls% $p='%param%'; """ & { $(try { iwr -useb %url% } catch { $p+= ' -m'; iwr -useb %url2% })} $p """" | iex
+
+pause
+exit /b
